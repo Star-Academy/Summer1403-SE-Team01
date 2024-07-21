08fb52d39153c9ac40c854ae9a3d96f117ab56b1
AmirReza <fgg32437@gmail.com>
2024-07-21 12:40:45 +0330

    Delete additonal file:)

--- project/obj/Debug/net8.0/project.AssemblyInfo.cs
+++ project/obj/Debug/net8.0/project.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("project")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4c12226c8fd74cd1f402942d6f7e5128fbcf71aa")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ba2e60592faf49956ff4ea00d3286721b5857c61")]
 [assembly: System.Reflection.AssemblyProductAttribute("project")]
 [assembly: System.Reflection.AssemblyTitleAttribute("project")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
