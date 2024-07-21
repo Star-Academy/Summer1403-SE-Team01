4c12226c8fd74cd1f402942d6f7e5128fbcf71aa
AmirReza <fgg32437@gmail.com>
2024-07-21 12:17:25 +0330

    finish with different implement:)

--- project/Program.cs
+++ project/Program.cs
@@ -1,112 +1,133 @@
-﻿/*P1*/
-using System;
+﻿using System.Runtime.Intrinsics.Arm;
 using System.Text.Json;
-class Student
-{
-  private int _StudentNumber;
-  public int StudentNumber   
-  {
-    get { return _StudentNumber; }
-    set { _StudentNumber = value; }
-  }
-  private string _FirstName;
-  public string FirstName   
-  {
-    get { return _FirstName; }
-    set { _FirstName = value; }
-  }
-  private string _LastName;
-  public string LastName
-  {
-    get { return _LastName; }
-    set { _LastName = value; }
-  }
-}
 
-class Scores
+namespace ScoreBoard
 {
-  private int _StudentNumber;
-  public int StudentNumber   
-  {
-    get { return _StudentNumber; }
-    set { _StudentNumber = value; }
-  }
-  private string _Lesson;
-  public string Lesson   
-  {
-    get { return _Lesson; }
-    set { _Lesson = value; }
-  }
-  private double _Score;
-  public double Score
-  {
-    get { return _Score; }
-    set { _Score = value; }
-  }  
-}
-
-class Result
-{
-    private int _StudentNumber;
-    public int StudentNumber   
+    public class Student
     {
-        get { return _StudentNumber; }
-        set { _StudentNumber = value; }
-    }
-
-     private double _Avg;
-  public double Avg   
-  {
-    get { return _Avg; }
-    set { _Avg = value; }
-  }
-}
-class Program 
-{
-    public static void Main(String[] args)
-    {
-        
-        JsonSerializerOptions _options = new()
+        private int _studentNumber;
+        public int StudentNumber
         {
-            PropertyNameCaseInsensitive = true
-        };
-        var json_students = File.ReadAllText("./files/students.json");
-        List<Student> students = JsonSerializer.Deserialize<List<Student>>(json_students, _options);
-        Console.WriteLine("**************************************",students.ElementAt(0).FirstName);
-        var json_scores = File.ReadAllText("./files/scores.json");
-        List<Scores> scores = JsonSerializer.Deserialize<List<Scores>>(json_scores, _options);
-
-        
-        int cur = 1, count = 0;
-        double sum = 0;
-        List<Result> res = new List<Result>();
-        foreach(var s in scores)
-        {
-            if(s.StudentNumber == cur) 
-            {
-                sum += s.Score;
-                count++;
-            
-            } 
-            else 
-            {
-                res.Add(new Result{StudentNumber = s.StudentNumber, Avg = (double)(sum/count)});
-                count = 0;
-                sum = 0;
-                cur++;
-            }
-        
+            get { return _studentNumber; }
+            set { _studentNumber = value; }
         }
 
-        //List<Result> ordered = res.OrderByDescending<Result>(x => x.Avg);
-        var ordered = res.OrderByDescending(x => x.Avg);
+        private string _firstName;
+        public string FirstName
+        {
+            get { return _firstName; }
+            set { _firstName = value; }
+        }
 
-        var s1 = students.Single(s=>s.StudentNumber==ordered.ElementAt(0).StudentNumber);
-        Console.WriteLine(ordered.ElementAt(0).Avg + " " + s1.FirstName + " " + s1.LastName);
-        var s2 = students.Single(s=>s.StudentNumber==ordered.ElementAt(1).StudentNumber);
-        Console.WriteLine(ordered.ElementAt(1).Avg + " " + s2.FirstName + " " + s2.LastName);
-        var s3 = students.Single(s=>s.StudentNumber==ordered.ElementAt(2).StudentNumber);
-        Console.WriteLine(ordered.ElementAt(2).Avg + " " + s3.FirstName + " " + s3.LastName);
-        
+        private string _lastName;
+        public string LastName
+        {
+            get { return _lastName; }
+            set { _lastName = value; }
+        }
+
+        private Dictionary<string, double> _courses = new Dictionary<string, double>();
+        public Dictionary<string, double> Courses
+        {
+            get { return _courses; }
+            set { _courses = value; }
+        }
+
+        private double _avg;
+        public double Avg
+        {
+            get { return _avg; }
+            set { _avg = value; }
+        }
+    }
+
+
+
+    public class Course
+    {
+        private string _name;
+        public string Name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+
+        private double _score;
+        public double Score
+        {
+            get { return _score; }
+            set { _score = value; }
+        }
+    }
+
+    public class StudentLesson
+    {
+        private int _studentNumber;
+        public int StudentNumber
+        {
+            get { return _studentNumber; }
+            set { _studentNumber = value; }
+        }
+
+        private string _lesson;
+        public string Lesson
+        {
+            get { return _lesson; }
+            set { _lesson = value; }
+        }
+
+        private double _score;
+        public double Score
+        {
+            get { return _score; }
+            set { _score = value; }
+        }
+    }
+
+
+
+    class Program
+    {
+        static void Main(String[] args)
+        {
+            JsonSerializerOptions _options = new()
+            {
+                PropertyNameCaseInsensitive = true
+            };
+
+            var json = File.ReadAllText("./files/students.json");
+            List<Student> students = JsonSerializer.Deserialize<List<Student>>(json, _options);
+
+            var json1 = File.ReadAllText("./files/scores.json");
+            List<StudentLesson> studentLessons = JsonSerializer.Deserialize<List<StudentLesson>>(json1, _options);
+
+            int count = 0, i = 0;
+            Student cur = students.ElementAt(i);
+            double sum = 0;
+
+            foreach(var sl in studentLessons) 
+            {
+                if(sl.StudentNumber != cur.StudentNumber) {
+                    cur.Avg = (double)(sum/count);
+                    if(++i<=students.Count)
+                        cur = students.ElementAt(i);
+                    count = 0;
+                    sum = 0;
+                }
+                cur.Courses.Add(sl.Lesson, sl.Score);
+                sum += sl.Score;
+                count++;
+            }
+
+            cur.Avg = (double)(sum / count);
+            var sorted = students.OrderByDescending(x=>x.Avg);
+            
+            Student s0 = sorted.ElementAt(0);
+            Student s1 = sorted.ElementAt(1);
+            Student s2 = sorted.ElementAt(2);
+            Console.WriteLine(s0.FirstName + " " + s0.LastName + " " + s0.Avg);
+            Console.WriteLine(s1.FirstName + " " + s1.LastName + " " + s1.Avg);
+            Console.WriteLine(s2.FirstName + " " + s2.LastName + " " + s2.Avg);
+        }
     }
 }
--- project/obj/Debug/net8.0/project.AssemblyInfo.cs
+++ project/obj/Debug/net8.0/project.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("project")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cd516988a540348291c0cb7bbb38f2d3d9260ed0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+994cb302b27276872a096002be9ab8b5ed8fb68d")]
 [assembly: System.Reflection.AssemblyProductAttribute("project")]
 [assembly: System.Reflection.AssemblyTitleAttribute("project")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
