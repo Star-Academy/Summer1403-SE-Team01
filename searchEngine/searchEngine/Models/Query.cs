cac64eb59ffd1d97769e9eba38df2b963f310ae4
HanaNrvtn <hanaNrvtn@gmail.com>
2024-07-22 00:55:00 -0700

    model classes implemented

---
+++ searchEngine/searchEngine/Models/Query.cs
@@ -0,0 +1,6 @@
+namespace WebApplication2.Models;
+
+public class Query
+{
+    public string query { get; set; }
+}
---
+++ searchEngine/searchEngine/Models/Search.cs
@@ -0,0 +1,7 @@
+namespace WebApplication2.Models;
+
+public class Search
+{
+    public Query query { get; set; }
+    public Result result { get; set; }
+}
